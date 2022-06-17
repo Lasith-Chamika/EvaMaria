7c5be57d7d6bd2d24bd8a5ce3e6e3c8bad347816
Lasith-Chamika <107011023+Lasith-Chamika@users.noreply.github.com>
2022-06-17 19:14:59 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -53,14 +53,13 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome']).delete()
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply(f"👋 Hey Welcome {u.mention} ,
-Welcome to <b>Cinema World.</>
-You Can Find Movies/Series/Animes & etc. From Here. Stay Safe & Enjoy...🌺
+                temp.MELCOW['welcome'] = await message.reply(f"👋 Hey {u.mention}, Welcome to <b>Cinema World.</b>
+You Can Find Movies/Series/Animes & Etc from here. Stay Safe & Enjoy...🌺
                                                              
 ┏≫ 𝙁𝙤𝙡𝙡𝙤𝙬 𝙂𝙧𝙤𝙪𝙥 𝙍𝙪𝙡𝙚𝙨
-┣ <b>ᴄʜᴀɴɴᴇʟ ››</b> <a href=https://t.me/Cinema_World_Sri_Lanka>ᴄɪɴᴇᴍᴀ ᴡᴏʀʟᴅ</a></b>
+┣ <b>ᴄʜᴀɴɴᴇʟ ››</b> <a href=https://t.me/Cinema_World_Sri_Lanka>ᴄɪɴᴇᴍᴀ ᴡᴏʀʟᴅ</a>
 ┗≫ 𝙁𝙤𝙡𝙡𝙤𝙬 𝙂𝙧𝙤𝙪𝙥 𝙍𝙪𝙡𝙚𝙨
-© <b>ᴏᴡɴᴇʀ ››</b> <a href=https://t.me/Cinema_World_Owner>ʟᴀsɪᴛʜ ᴄʜᴀᴍɪᴋᴀ</a></b>")
+© <b>ᴏᴡɴᴇʀ ››</b> <a href=https://t.me/Cinema_World_Owner>ʟᴀsɪᴛʜ ᴄʜᴀᴍɪᴋᴀ</a>")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
